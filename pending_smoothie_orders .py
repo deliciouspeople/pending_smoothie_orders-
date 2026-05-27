df3fc499cd870d1c16773cefb84a05ac5327e367
deliciouspeople <test11tesuto22test@gmail.com>
2026-05-27 11:04:25 +0900

    Update pending_smoothie_orders .py
    APIの修正

--- pending_smoothie_orders .py
+++ pending_smoothie_orders .py
@@ -36,5 +36,5 @@ else:
 
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response)
